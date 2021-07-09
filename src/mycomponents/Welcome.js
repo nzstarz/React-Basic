dc750da115d5fc14ccb2f887b4e27f78aa36182f
nzstarz <nazmul.hasan@northsouth.edu>
2021-07-10 01:05:19 +0600

    Practicing Class Component

---
+++ src/mycomponents/Welcome.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react'
+
+//Practicing Class Component
+class Welcome extends Component{
+    render(){
+        return <h1> And Now you see me from Class Component</h1>
+    }
+}
+export default Welcome;
