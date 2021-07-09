57bc77791e7832eccf48df03112f61bc8557069c
nzstarz <nazmul.hasan@northsouth.edu>
2021-07-10 00:46:35 +0600

    React Functional Component

---
+++ src/mycomponents/Hello.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+//Practicing Functional Component   
+function Hello(){
+    return(
+        <h1> This is Nazmul Hasan from Functional Component</h1>
+    )
+}
+export default Hello;
