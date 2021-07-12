ae2f1332cbaae525aeb8dfbdb65a8b3ea0229485
nzstarz <nazmul.hasan@northsouth.edu>
2021-07-13 00:28:30 +0600

    Nested React State And Array

--- src/mycomponents/Welcomestate.js
+++ src/mycomponents/Welcomestate.js
@@ -1,4 +1,4 @@
-//React State where you store property values
+//React State where you store property values, Nested React State And Array In Property
 import React, {Component} from 'react'
 
 class Welcomesate extends Component{
@@ -8,9 +8,14 @@ class Welcomesate extends Component{
 
         this.state={
             job : "I'm Practicing React State ",
-            salary : "32K",
-            company : "b71",
-            address : "MirpurDOHS"
+            salary : ["32K", "25K", "35K", "40K"],
+            address : "Mirpur DOHS" ,
+            company : {
+                        C1: ["b71", "Bkash", "Nagad" ],
+                        C2: "Evaly", 
+                        C3:  "Evercare"
+                     }
+            
 
         }
     }
@@ -19,8 +24,8 @@ class Welcomesate extends Component{
     render(){
         return <div>
             <h1>Whats your Job = {this.state.job}</h1>
-            <h1> How Much They Pay = {this.state.salary}</h1>
-            <h1>Whats the Name of the company = {this.state.company}</h1>
+            <h1> How Much They Pay = {this.state.salary[3]}</h1>
+            <h1>Whats the Name of the company = {this.state.company.C1[2]}</h1>
             <h1>Where is it = {this.state.address}</h1>
             </div>
     }
