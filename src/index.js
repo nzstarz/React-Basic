7a4490d40f2bb6de30ef74c2dcbd52301da4b184
nzstarz <nazmul.hasan@northsouth.edu>
2021-07-31 00:27:31 +0600

    React Routing Practice

--- src/index.js
+++ src/index.js
@@ -3,12 +3,40 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Portfolio from './pages/Portfolio'
+import {Route, Link, BrowserRouter as Router} from '../node_modules/react-router-dom'
+
+//React Routing Practice
+const myrouter=(
+  <Router>
+    
+  <div>
+    <ul>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/contact">Contact</Link>
+      <Link to="/portfolio">Portfolio</Link>
+
+    </ul>
+
+    <Route exact path="/" component={App}/>
+    <Route path="/about" component={About}/>
+    <Route path="/contact" component={Contact}/>
+    <Route path="/portfolio" component={Portfolio}/>
+
+
+  </div>
+
+  </Router>
+)
+
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+ myrouter,  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
