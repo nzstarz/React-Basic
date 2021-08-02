89baff34187de9ac2a4cf7aa9a8b2a9f25556538
nzstarz <nazmul.hasan@northsouth.edu>
2021-08-02 16:44:27 +0600

    Http Get Data And Render With map()

--- src/mycomponents/CountryList.js
+++ src/mycomponents/CountryList.js
@@ -3,14 +3,19 @@ import React, { Component } from 'react';
 import axios from 'axios'
 
 class CountryList extends Component {
+    constructor(){
+        super();
+        this.state={
+            mydata: []
+        }
+    }
 
 // Use componentDidMount method from react lifecycle to get data through http request from server
     componentDidMount(){
         //Axios is a promise based HTTP client for the browser and Node. js
         axios({method: 'get', url:'https://restcountries.eu/rest/v2/all'})
         .then(response=>{
-            console.log(response.data)
-
+            this.setState({mydata:response.data})
         })
         .catch(error=>{
             console.log(error)
@@ -19,9 +24,15 @@ class CountryList extends Component {
 
     }
     render() {
+        const mylist=this.state.mydata
+        const CountryName = mylist.map((mylist)=>{
+            return <li>{mylist.name}</li>
+
+        })
+
         return (
             <div>
-                
+                <ol>{CountryName}</ol>
             </div>
         );
     }
